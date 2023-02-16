495270310135c6e877885def5336c083003ef598
clairefro <claire.froelich@gmail.com>
2023-02-16 14:21:34 -0500

    Add prompt and usage commands

---
+++ config.js
@@ -0,0 +1,24 @@
+/** Config */
+require("dotenv").config();
+
+const config = {
+  openAiApiKey: process.env.OPENAI_API_KEY,
+};
+
+/** Check for required env vars, exit script if missing */
+const checkEnvVars = () => {
+  const required = ["OPENAI_API_KEY"];
+  const missing = [];
+
+  required.forEach((key) => {
+    if (!process.env[key]) {
+      missing.push(key);
+    }
+  });
+  if (missing.length) {
+    console.error(`Missing the following config: ${missing.join(", ")}`);
+    process.exit(1);
+  }
+};
+
+module.exports = { config, checkEnvVars };
--- index.js
+++ index.js
@@ -1,19 +1,77 @@
 const program = require("commander");
+const { openai } = require("./lib/openai");
+const { checkEnvVars } = require("./config");
+const package = require("./package.json");
+const { writeDataToFileSync } = require("./lib/writeDataToFile");
+
+checkEnvVars();
 
 program
-  .version("1.0.0")
+  .version(package.version)
   .description("A simple node CLI shell script boilerplate")
   .option("-f, --flag", "An example flag")
-  .arguments("<args>")
+  //   .arguments("<args>")
   .action((args) => {
-    console.log(`Received args: ${args}`);
+    console.log(program.version());
   });
 
 program
-  .command("example <name>")
-  .description("An example command")
-  .action((name) => {
-    console.log(`Hello, ${name}!`);
+  .command("usage")
+  .alias("u")
+  .description("Get OpenAI API usage information for the configured API KEY")
+  .action(async () => {
+    try {
+      const res = await openai.usage();
+      if (res.status > 199 && res.status < 299) {
+        // success
+        console.log("Response:");
+        console.log(res.data);
+      } else {
+        console.log(res.data);
+        // something went wrong
+      }
+    } catch (e) {
+      console.error(e);
+      //   console.log(e.response.data);
+      process.exit(1);
+    }
+  });
+
+program
+  .command("generate")
+  .alias("g")
+  .option("-p, --prompt <prompt>", "Prompt for OpenAI")
+  .option("-o, --out <out>", "Path for output file")
+  .description(
+    "Generate anything from a prompt. Optionally output to a file with -o <path>"
+  )
+  .action(async (args) => {
+    try {
+      //   if (!!args.out) {
+      //     writeDataToFileSync("foop", args.out);
+      //   }
+
+      const res = await openai.completion(args.prompt);
+      if (res.status > 199 && res.status < 299) {
+        // success
+
+        const data = res.data;
+
+        const text = data.choices[0].text;
+
+        if (!!args.out) {
+          console.log(`Writing output to ${args.out}`);
+          writeDataToFileSync(text.trim(), args.out);
+        }
+        console.log("Response:");
+        console.log(text);
+      } else {
+        // something went wrong
+      }
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
